222d04571500ba85908a2b87182fe1ce77a8d6e2
Otto Fajardo <otto.fajardob@gmail.com>
2026-04-28 17:52:28 +0200

    [skip ci] adding checking for pandas or polars installed

--- pyreadr/__init__.py
+++ pyreadr/__init__.py
@@ -1,3 +1,20 @@
+_has_pandas = False
+_has_polars = False
+try:
+    import pandas
+    _has_pandas = True
+except ImportError:
+    pass
+try:
+    import polars
+    _has_polars = True
+except ImportError:
+    pass
+
+if not _has_pandas and not _has_polars:
+    raise ImportError("pyreadr requires either pandas or polars to be installed. "
+                      "Please install at least one of them.")
+
 from .pyreadr import read_r, list_objects, write_rds, write_rdata, download_file
 from .custom_errors import PyreadrError, LibrdataError
 
